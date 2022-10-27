1e163d7444c60a403297151657496f5b94c73695
Alice Agrest <114485470+AliceAgrest@users.noreply.github.com>
2022-10-27 09:34:29 +0300

    Create Program.cs

--- proj3/Program.cs
+++ proj3/Program.cs
@@ -6,7 +6,21 @@ namespace proj3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //answer 1
+            int numcount = 0 ;
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("The divisible number with 3 and 7 is "+ i);
+                    numcount++;
+                }
+                //Console.WriteLine("the count numbers that divisble with 3 and 7 between 1-100 are " + numcount);
+            }
+            Console.WriteLine("the count numbers that divisble with 3 and 7 between 1-100 are " + numcount);
+
+            Console.ReadLine();
         }
     }
 }
