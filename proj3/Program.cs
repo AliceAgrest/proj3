fbe62d109e67436919a6c56f03b6b9b1480c7bc7
Alice Agrest <114485470+AliceAgrest@users.noreply.github.com>
2022-10-30 16:04:08 +0200

    Create Program.cs

--- proj3/Program.cs
+++ proj3/Program.cs
@@ -19,6 +19,35 @@ namespace proj3
             }
             Console.WriteLine("the count numbers that divisble with 3 and 7 between 1-100 are " + numcount);
 
+             //answer9
+            int n, a;
+
+            Console.WriteLine("Enter a number for loop ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number ");
+            a = int.Parse(Console.ReadLine());
+
+            bool flag = true;
+           // bool flag1 = false;
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("Enter a number ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (a <= x)
+                    flag = true;
+                else 
+                    flag = false;
+
+                 a = x;
+            }
+            if (flag == true)
+                Console.WriteLine("Up set");
+            else if (flag == false)
+                Console.WriteLine("Down set");
+            else
+                Console.WriteLine("None set");
+
             Console.ReadLine();
         }
     }
