7d6d562ad6e54969aed387b3079361f6900e0d84
Alice Agrest <114485470+AliceAgrest@users.noreply.github.com>
2022-10-30 21:19:50 +0200

    Update Program.cs

--- proj3/Program.cs
+++ proj3/Program.cs
@@ -5,8 +5,8 @@ namespace proj3
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //answer 1
+        {//answer1
+
             int numcount = 0 ;
             
             for (int i = 1; i <= 100; i++)
@@ -18,8 +18,108 @@ namespace proj3
                 }
             }
             Console.WriteLine("the count numbers that divisble with 3 and 7 between 1-100 are " + numcount);
+            
+            //answer2
+            int numcount2 = 0, a;
 
-             //answer9
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine("Enter a number ");
+                a = int.Parse(Console.ReadLine());
+
+                if (a % 2 == 0)
+                    numcount2++;
+            }
+            Console.WriteLine("Total even number is " + numcount2);
+            
+            //answer3
+            int k, n;
+
+            Console.WriteLine("Enter a big number ");
+            k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a small number ");
+            n = int.Parse(Console.ReadLine());
+            for (int c = n; c <= k; c++)
+            {
+                Console.WriteLine(" " + c);
+            }
+            
+            //answer4
+            int n, numcounteven = 0, numcount = 0;
+            double remainder;
+
+            Console.WriteLine("Enter a number ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1;i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    numcounteven++;
+                else
+                    numcount++;
+            }
+            Console.WriteLine($"Total even number {numcounteven} and total odd number {numcount}");
+            remainder = (double)numcounteven / numcount;
+            Console.WriteLine($"The remainder of even numbers and odd numbers is {remainder}");
+            
+            //answer5
+            int n, total = 1;
+
+            Console.WriteLine("Enter a number ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i != n + 1; i ++)
+            {
+                total = i * total;
+            }
+            Console.WriteLine($"The total number is {total}");
+            
+            //answer6
+            int n, sign = -1;
+
+            Console.WriteLine("Enter a number ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1;i <= n; i++)
+            {
+                sign = sign * -1;
+                Console.WriteLine(" " + (i * sign));
+            }
+
+            //answer7
+            int n, a = 0, b = 1, res;
+
+            Console.WriteLine("Enter a number ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}");
+            Console.WriteLine($"{b}");
+            res = a + b;
+            Console.WriteLine($"{res}");
+            a = b;
+            b = res;
+            for (int x = 1; x < n; x++)
+            {
+                res = a + b;
+                Console.WriteLine($"{res}");
+                a = b;
+                b = res;
+            }
+
+            //answer8
+            int n;
+            int isPrimeNumber = 0;
+
+            Console.WriteLine("Enter a number ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) {
+                    Console.WriteLine("0");
+                    isPrimeNumber = 1;
+                    break;
+                }
+            }
+            if(isPrimeNumber == 0)
+                Console.WriteLine("1");
+
+            //answer9
             int n, a;
 
             Console.WriteLine("Enter a number for loop ");
@@ -28,7 +128,6 @@ namespace proj3
             a = int.Parse(Console.ReadLine());
 
             bool flag = true;
-           // bool flag1 = false;
             for (int i = 0; i <= n; i++)
             {
                 Console.WriteLine("Enter a number ");
@@ -36,10 +135,10 @@ namespace proj3
 
                 if (a <= x)
                     flag = true;
-                else 
+                else
                     flag = false;
 
-                 a = x;
+                a = x;
             }
             if (flag == true)
                 Console.WriteLine("Up set");
